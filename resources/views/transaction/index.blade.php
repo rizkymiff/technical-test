1a2075eb2d6856eed992b8a46c2ea7362d272f2c
Ach Miftaqul Rizky <tizqy.miftaqul77@gmail.com>
2022-05-25 21:08:45 +0700

    update: add breadcrumbs

--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -2,6 +2,25 @@
 
 @section('title', 'Data transaksi')
 
+@section('breadcrumb')
+<div class="content-header">
+    <div class="d-flex align-items-center">
+        <div class="mr-auto">
+            <h3 class="page-title">Data transaksi</h3>
+            <div class="d-inline-block align-items-center">
+                <nav>
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}"><i class="mdi mdi-home-outline"></i></a></li>
+                        <li class="breadcrumb-item active" aria-current="transaction">Transaksi</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
+
 @section('content')
     <div class="box">
         <div class="box-header with-border">
