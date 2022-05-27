4dbe4a0b65c2b749f49f541b70693d00e678cf27
Ach Miftaqul Rizky <tizqy.miftaqul77@gmail.com>
2022-05-27 10:41:07 +0700

    add: mutator property

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getIsAdminAttribute()
+    {
+        return $this->role == 'admin';
+    }
+
+    public function getIsUserAttribute()
+    {
+        return $this->role == 'user';
+    }
 }
