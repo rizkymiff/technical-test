d22056dae5b6a95f3eb757848a61280ca005de3e
Ach Miftaqul Rizky <tizqy.miftaqul77@gmail.com>
2022-05-27 10:42:29 +0700

    add: Transaction policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Transaction;
+use App\Policies\TransactionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,6 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
+        Transaction::class => TransactionPolicy::class,
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
